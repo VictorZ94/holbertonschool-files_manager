fc54f80647640283c766786ff598a52389975c2b
Victor Zuluaga <zrvictor@outlook.com>
2021-10-12 13:36:09 -0500

    redis client connection

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import { promisify } from 'util';
+
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(err.message));
+  }
+
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value);
+    await this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const client = new RedisClient();
+export default client;
